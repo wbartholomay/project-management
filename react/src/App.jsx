22fab9fa4da449b6e097cea6153687ce4a55135e
Zach Florian <zflorian@travelers.com>
2024-07-11 12:44:29 -0400

    setting up home page

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -14,9 +14,93 @@ function App() {
   return (
     <>
     <Router>
-      
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            TSE
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link className="dropdown-item" to="/add">
+                      Add Sock
+                    </Link>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="card-container">
+            <AuthProvider>
+            <Routes>
+                <Route exact path="/" element={<Start></Start>}
+                <Route path="/about" element={<About />} />
+                <Route path="/add" element={
+                } />
+                <Route path="/Login" element={<LoginForm />} />
+            </Routes>
+            </AuthProvider>
+              <Footer environment={"development"}></Footer>
+            </div>
+          </div>
+        </div>
+      </main>
     </Router>
-    <Login></Login>
     </>
   )
 }
