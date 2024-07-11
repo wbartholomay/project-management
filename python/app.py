d2890e5505895dd888f0a4927edf4d2e2aa27719
Will Bartholomay <william.bartholomay@uconn.edu>
2024-07-11 13:08:57 -0400

    generate_result()

--- python/app.py
+++ python/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import pandas as pd
 import pickle
 
 app = Flask(__name__)
@@ -8,4 +9,11 @@ with open("model.pkl", "rb") as file:
 @app.route('/model', methods = ["GET"])
 def generate_result():
     body = request.args
-    
+
+    X = pd.DataFrame({"teamSize" : body.get("teamSize"), "budget" : body.get("budget"), "workload" : body.get("workload")})
+
+    predicted_completion_time = model.predict(X)
+
+    return {"daysToComplete" : predicted_completion_time}
+
+if __name__ == 
