80d0061e0f2aace42639b718d178bc8f7b47c9a3
Will Bartholomay <william.bartholomay@uconn.edu>
2024-07-11 13:26:10 -0400

    added model, finished flask

--- python/app.py
+++ python/app.py
@@ -3,17 +3,25 @@ import pandas as pd
 import pickle
 
 app = Flask(__name__)
-with open("model.pkl", "rb") as file:
+with open("python/model.pkl", "rb") as file:
     model = pickle.load(file)
 
-@app.route('/model', methods = ["GET"])
+@app.route('/', methods = ["POST"])
 def generate_result():
-    body = request.args
+    body = request.json
 
-    X = pd.DataFrame({"teamSize" : body.get("teamSize"), "budget" : body.get("budget"), "workload" : body.get("workload")})
 
-    predicted_completion_time = model.predict(X)
+    team_size = body.get("teamSize")
+    budget = body.get("budget")
+    workload = body.get("workload")
 
-    return {"daysToComplete" : predicted_completion_time}
+    X = pd.DataFrame([{"teamSize" : team_size , "budget" : budget, "workload" : workload}])
 
-if __name__ == 
+    predicted_completion_time = model.predict(X)[0]
+
+    # return {}
+
+    return {"daysToComplete" : int(predicted_completion_time)}
+
+if __name__ == "__main__":
+    app.run(debug=True)
