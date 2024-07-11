c0755cd82cd06bad303087432287a355f6776539
Zach Florian <zachary.florian@uconn.edu>
2024-07-11 11:48:30 -0400

    routing

---
+++ react/src/components/Start.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Start() {
+  return (
+    <>
+      <header>
+        <div>Project Management</div>
+      </header>
+      <body>
+        
+      </body>
+    </>
+  )
+}
