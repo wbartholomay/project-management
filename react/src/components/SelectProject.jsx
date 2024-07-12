ff1c81fa3055dbbebee6529512790d76c0a3f555
Zach Florian <zflorian@travelers.com>
2024-07-12 10:37:47 -0400

    empty list of projects

--- react/src/components/SelectProject.jsx
+++ react/src/components/SelectProject.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import dotenv from 'dotenv'
 export default function SelectProject() {
-  const user = "jsmith";
+  const user = "a";
   const [projectList, setProjectList] = useState([]);
 
   useEffect(() => {
@@ -19,19 +19,26 @@ export default function SelectProject() {
   return (
     <>
       <div>Your Projects</div>
-
-      {projectList.map((proj) => (
-        <Link key={proj._id} to="/Project">
-          <div className="card card-container d-flex flex-row justify-content-start">
-            <div className="project-name">{proj.name}</div>
-            <div className="project-attributes">Team Size: {proj.teamSize}</div>
-            <div className="project-attributes">Workload: {proj.workload}</div>
-            <div className="project-attributes">
-              Estimated Days to Completion:{proj.daysToComplete}
+      {projectList.length === 0 ? (
+        <div>No projects found. Please create a new project!</div>
+      ) : (
+        projectList.map((proj) => (
+          <Link key={proj._id} to="/Project">
+            <div className="card card-container d-flex flex-row justify-content-start">
+              <div className="project-name">{proj.name}</div>
+              <div className="project-attributes">
+                Team Size: {proj.teamSize}
+              </div>
+              <div className="project-attributes">
+                Workload: {proj.workload}
+              </div>
+              <div className="project-attributes">
+                Estimated Days to Completion: {proj.daysToComplete}
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))
+      )}
     </>
   );
 }
