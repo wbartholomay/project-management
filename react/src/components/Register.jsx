9f3628fe632d1aa19cf1192464efab5978a090dd
Will Bartholomay <william.bartholomay@uconn.edu>
2024-07-12 10:28:21 -0400

    skeleton done

--- react/src/components/Register.jsx
+++ react/src/components/Register.jsx
@@ -1,25 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    if (password == confirmedPassword) {
+      alert("Success");
+    } else {
+      alert("Passwords do not match.");
+    }
+    console.log(username, password);
+  };
+
   return (
     <>
-        <div id='register-card' className='card'>
+      <div id="register-card" className="card">
         <h3>Register</h3>
         <form>
-            <label id='username-label' htmlFor='username'>Username:</label>
-            <input id="username" type="text" />
-            <br></br>
-            <br></br>
-            <label id='password-label' htmlFor='password'>Password:</label>
-            <input id="password" type="password" />
-            <br /><br />
-            <label id='confirm-password-label' htmlFor='confirm-password'>Confirm Password:</label>
-            <input id="confirm-password" type="password" />
-            <br /><br />
-            <Link to="/login">Already have an account? Login here</Link>
+          <label id="username-label" htmlFor="username">
+            Username:
+          </label>
+          <input
+            id="username"
+            type="text"
+            placeholder="Enter Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <br></br>
+          <br></br>
+          <label id="password-label" htmlFor="password">
+            Password:
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <br />
+          <label id="confirm-password-label" htmlFor="confirm-password">
+            Confirm Password:
+          </label>
+          <input
+            id="confirm-password"
+            type="password"
+            placeholder="Password"
+            value={confirmedPassword}
+            onChange={(e) => setConfirmedPassword(e.target.value)}
+          />
+          <br />
+          <br />
+          <Link to="/login">Already have an account? Login here</Link>
         </form>
-        </div>
+      </div>
     </>
-  )
+  );
 }
