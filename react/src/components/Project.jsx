fbe22bc966a5be61dffb6ee887e2770be194e215
Zach Florian <zflorian@travelers.com>
2024-07-12 13:40:17 -0400

    display cards relating to project

--- react/src/components/Project.jsx
+++ react/src/components/Project.jsx
@@ -5,7 +5,6 @@ import Task from './Task'
 const Project = () => {
   const [taskList, setTaskList] = useState([])
   const location = useLocation();
-  console.log(location)
   const project = location.state || {};
   console.log(project)
   useEffect(() => {
@@ -24,12 +23,34 @@ const Project = () => {
   }
 
   return (
-    <div>
-      <h1>{project.name}</h1>
-      <p>Team Size: {project.teamSize}</p>
-      <p>Workload: {project.workload}</p>
-      <p>Estimated Days to Completion: {project.daysToComplete}</p>
-    </div>
+    <>
+      <div>
+        <h1>{project.name}</h1>
+        <p>Team Size: {project.teamSize}</p>
+        <p>Workload: {project.workload}</p>
+        <p>Estimated Days to Completion: {project.daysToComplete}</p>
+      </div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6 bg-light border">
+            <h4 className="text-center">To Do</h4>
+            {taskList
+              .filter((task) => !task.isComplete)
+              .map((task) => (
+                <Task key={task._id} task={task} />
+              ))}
+          </div>
+          <div className="col-md-6 bg-light border">
+            <h4 className="text-center">Complete</h4>
+            {taskList
+              .filter((task) => task.isComplete)
+              .map((task) => (
+                <Task key={task._id} task={task} />
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
