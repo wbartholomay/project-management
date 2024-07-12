1836db57b93c89ef9cc9079beca1f318db2c3dce
Zach Florian <zflorian@travelers.com>
2024-07-12 11:02:47 -0400

    trying to send projectList data to project page

--- react/src/components/Project.jsx
+++ react/src/components/Project.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-export default function Project(props) {
+const Project = () => {
+  const location = useLocation();
+  console.log(location)
+  const { project } = location.state || {};
+  useEffect(() => {
+    console.log(location)
+  }, [location]);
+  if (!project) {
+    return <div>No project data available.</div>;
+  }
 
   return (
-    <div>Project</div>
-  )
-}
+    <div>
+      <h1>{project.name}</h1>
+      <p>Team Size: {project.teamSize}</p>
+      <p>Workload: {project.workload}</p>
+      <p>Estimated Days to Completion: {project.daysToComplete}</p>
+    </div>
+  );
+};
+
+export default Project;
--- react/src/components/SelectProject.jsx
+++ react/src/components/SelectProject.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import dotenv from 'dotenv'
 export default function SelectProject() {
-  const user = "a";
+  const user = "jsmith";
   const [projectList, setProjectList] = useState([]);
 
   useEffect(() => {
@@ -23,7 +23,7 @@ export default function SelectProject() {
         <div>No projects found. Please create a new project!</div>
       ) : (
         projectList.map((proj) => (
-          <Link key={proj._id} to="/Project">
+          <Link key={proj._id} to='/Project' state={proj}>
             <div className="card card-container d-flex flex-row justify-content-start">
               <div className="project-name">{proj.name}</div>
               <div className="project-attributes">
