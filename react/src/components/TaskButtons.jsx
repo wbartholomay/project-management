1397c29c274e6fd0511d77b013490f7b9b1357d7
Zach Florian <zflorian@travelers.com>
2024-07-15 15:52:33 -0400

    more css changes

--- react/src/components/TaskButtons.jsx
+++ react/src/components/TaskButtons.jsx
@@ -6,19 +6,24 @@ export default function TaskButtons({ task, taskList, setTaskList, handleEditTas
         <div className="task-buttons">
           <button
             onClick={() => handleEditTaskPopup(task._id)}
-            className="edit-button"
+            className="edit-button button-secondary"
           >
             Edit
           </button>
           <button
             onClick={() => handleDeleteTask(task._id, setTaskList)}
-            className="delete-button"
+            className="delete-button button-secondary"
           >
             Delete
           </button>
         </div>
         <div className="task-switch">
-          <button onClick={() => handleSwitchTask(task._id, task, taskList, setTaskList)} className="switch-button">
+          <button
+            onClick={() =>
+              handleSwitchTask(task._id, task, taskList, setTaskList)
+            }
+            className="switch-button button-secondary"
+          >
             Switch
           </button>
         </div>
