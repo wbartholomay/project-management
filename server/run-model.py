f3ab15402f28df2981cba2da5fd3d206a67fbc24
Will Bartholomay <william.bartholomay@uconn.edu>
2024-07-16 12:29:18 -0400

    added comments

--- server/run-model.py
+++ server/run-model.py
@@ -4,13 +4,18 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
 if __name__ == '__main__':
+    #take command line input for features
     team_size = int(sys.argv[1])
     budget = int(sys.argv[2])
     workload = int(sys.argv[3])
 
+    #load model
     with open("model.pkl", "rb") as file:
         model = pickle.load(file)
 
+    #load data into dataframe
     X = pd.DataFrame({"teamSize" : [team_size], "budget" : [budget], "workload": [workload]})
+    #predict result
     pred = model.predict(X)
+    #output prediction
     print(pred[0])
